81df14bb8bda37bdff952fe7f9a1e89a1466fc7d
Jorge L. Cossi <jcos201@gmail.com>
2021-01-09 16:36:52 -0500

    adds footer styling

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -2,7 +2,7 @@ import styles from './Footer.module.css';
 
 function Footer(props) {
     return (
-    <footer>
+    <footer className={styles.Footer}>
         <p>Copyright &copy; All rights reserved {new Date().getFullYear()} Application Tracker</p>
     </footer>);
 };
