a040dbea2af62124a22bc59434d582456713a9b4
Jorge L. Cossi <jcos201@gmail.com>
2021-01-09 17:28:59 -0500

    adds basic csr

--- src/App.js
+++ src/App.js
@@ -1,13 +1,34 @@
 import Header from './components/Header';
 import Footer from './components/Footer';
+
+import HomePage from './pages/HomePage';
+import SignupPage from './pages/SignupPage';
+import LoginPage from './pages/LoginPage';
+import DashboardPage from './pages/DashboardPage';
+
+import { Route, Switch} from 'react-router-dom';
 import './App.css';
+
 
 
 function App() {
   return (
     <div className="App">
       <Header/>
-      <main>Job Application Tracker</main>
+      <main><Switch>
+          <Route exact path="/" render={props =>
+            <HomePage/>
+          } />
+          <Route exact path="/dashboard" render={props =>
+            <DashboardPage />
+          } />
+          <Route exact path="/signup" render={props =>
+            <SignupPage/>
+          } />
+          <Route exact path="/login" render={props =>
+            <LoginPage/>
+          } />
+        </Switch></main>
       <Footer/>
     </div>
   );
