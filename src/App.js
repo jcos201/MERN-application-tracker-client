5863251186fe910d21324747d540de7e6fe74518
Jorge L. Cossi <jcos201@gmail.com>
2021-01-09 16:17:42 -0500

    imports header and footer

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
+import Header from './components/Header';
+import Footer from './components/Footer';
 import './App.css';
+
 
 function App() {
   return (
     <div className="App">
+      <Header/>
       <p>Job Application Tracker</p>
+      <Footer/>
     </div>
   );
 }
