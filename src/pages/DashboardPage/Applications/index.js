9708c91bca3f2aecfd761dd55bb6c8d7b6b5bf65
Jorge L. Cossi <jcos201@gmail.com>
2021-01-13 20:39:22 -0500

    added StoredApplications and AddApplication pages

--- src/pages/DashboardPage/Applications/index.js
+++ src/pages/DashboardPage/Applications/index.js
@@ -2,11 +2,15 @@ import { Link } from 'react-router-dom'
 
 
 function ApplicationsPage (props) {
-
+    const allApplications = props.user.applications;
     return (
         <div>
         <div>{props.user.firstName}'s Job Applications Page</div>
         <div>Number of Job Applcations: {props.user.applications.length}</div>
+        {allApplications.length > 0 ? 
+        <p>There are applications here</p> 
+    :
+        <p>There are no applications here</p>}
         <Link to="/addApplication"><button>Add Application</button></Link>
         </div>
     )
