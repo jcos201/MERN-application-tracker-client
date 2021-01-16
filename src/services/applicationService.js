4f76bb4618e300c51c0f52cf1f9f326b59affe7d
Jorge L. Cossi <jcos201@gmail.com>
2021-01-16 10:59:54 -0500

    capturing updated applications array data

--- src/services/applicationService.js
+++ src/services/applicationService.js
@@ -1,3 +1,4 @@
+import { updateUserApplications } from './userService'
 const BASE_URL = 'http://localhost:3001/users';
 
 function addListing (applicationInfo) {
@@ -10,10 +11,27 @@ function addListing (applicationInfo) {
     }).then( response => {
         if(response.ok) return response.json();
         throw new Error ('Bad Application Data');
-    }).then (data => console.log(data))
+        
+    }).then (data => {
+        console.log('data coming back from express');
+        console.log(JSON.stringify(data.applicationArray));
+        const applications = JSON.stringify(data.applicationArray);
+        updateUserApplications(applications);
+    })
+};
+
+
+function getListings(userId) {
+    const headers = {
+        'Content-Type': 'Application/json'
+    }
+    return fetch(BASE_URL + '/applications/' + userId, { headers })
+        .then(response => response.json())
+        .then(data => console.log(JSON.stringify(data)));
 }
 
 
 export {
     addListing,
+    getListings
 }
