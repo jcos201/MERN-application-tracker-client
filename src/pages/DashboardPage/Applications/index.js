ea8806ebbe40a246b769c657ccfe8402cde3183a
Jorge L. Cossi <jcos201@gmail.com>
2021-01-16 13:43:09 -0500

    added application GET request and link to add applications to DB

--- src/pages/DashboardPage/Applications/index.js
+++ src/pages/DashboardPage/Applications/index.js
@@ -1,14 +1,41 @@
 import { Link } from 'react-router-dom'
-
+import { useState, useEffect } from 'react'
+import { getUser } from '../../../services/userService'
+import { getToken } from '../../../services/tokenService';
+const BASE_URL = 'http://localhost:3001/users';
 
 function ApplicationsPage (props) {
-    const allApplications = props.user.applications;
-    console.log(props.user)
+    const [applicationsState, setApplicationsState] = useState([]);
+
+    //console.log(props.user)
+
+
+    useEffect(() => {
+        const userId = getUser()._id;
+        const requestOptions = {
+            headers: { 
+                'Content-Type': 'Application/json',
+                'Authorization': 'Bearer ' + getToken() },
+        }
+        fetch(BASE_URL + '/applications/' + userId, requestOptions)
+            .then(response => response.json())
+            .then(data => setApplicationsState(JSON.stringify(data.applicationArray))).then(() => {
+
+                console.log('applicationState after fetch:');
+                console.log(applicationsState)
+                console.log('finished Fetch')
+            }
+            );
+        
+    }, [])    
+
     return (
-        <div>
+        <>
+        <Link to="/addApplication"><button>Add Application</button></Link>
         <div>{props.user.firstName}'s Job Applications Page</div>
-        <div>Number of Job Applcations: {props.user.applications.length}</div>
-        {allApplications.length > 0 ? 
+        <div>Number of Job Applcations: {applicationsState.length}</div>
+        {applicationsState.length > 0 ? 
+        <tbody>
         <table>
             <tr>
             <th>Company Name</th>
@@ -22,10 +49,10 @@ function ApplicationsPage (props) {
 
             </tr>
         </table>
+        </tbody>
             :
         <p>There are no applications here</p>}
-        <Link to="/addApplication"><button>Add Application</button></Link>
-        </div>
+        </>
     )
 }
 
