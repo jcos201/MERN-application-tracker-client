24bb2547497dd5a2af117579509c24432fcd2cb2
Jorge L. Cossi <jcos201@gmail.com>
2021-01-19 23:06:26 -0500

    cleaned up code

--- src/pages/DashboardPage/index.js
+++ src/pages/DashboardPage/index.js
@@ -1,13 +1,9 @@
 import { Link } from 'react-router-dom'
 
-
-
 function DashboardPage (props) {
     return(
         <div>
             {props.user.firstName}'s Dashboard Page
-            <br/>{props.user._id}
-            <br/>{props.user.applications.length}<br/>
             <Link to="/applications"><button>Applications</button></Link>
             <Link to="/jobsearch"><button>My Job Search</button></Link>
         </div>
