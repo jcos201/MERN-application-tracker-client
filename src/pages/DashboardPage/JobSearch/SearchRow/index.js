b1fbffa81287ce8f94915187be21e2de39dff396
Jorge L. Cossi <jcos201@gmail.com>
2021-01-20 19:45:01 -0500

    added callback function to parent component

--- src/pages/DashboardPage/JobSearch/SearchRow/index.js
+++ src/pages/DashboardPage/JobSearch/SearchRow/index.js
@@ -1,7 +1,15 @@
 import { Link } from 'react-router-dom'
+import { getToken } from '../../../../services/tokenService'
+
+const BASE_URL = 'http://localhost:3001/users';
 
 function SearchRow(props){
     let id = props.searchId;
+
+    function onTrigger (event) {
+        event.preventDefault()
+        props.handleDelete(id)
+    }
 
     return(
             <tr>
@@ -9,6 +17,7 @@ function SearchRow(props){
             <th>{props.city}</th>
             <th>{props.state1}</th>
             <td><Link to={"/search/"+id}><button >Search</button></Link></td>
+            <td><button onClick={onTrigger}>Delete</button></td>
             </tr>
     );
 }
