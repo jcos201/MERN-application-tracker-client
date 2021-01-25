b892d8fc5b0f18d3000d4553a909967714eb0a5b
Jorge L. Cossi <jcos201@gmail.com>
2021-01-25 16:41:08 -0500

    updated Navbar to bootstrap version

--- src/components/Navigation/index.js
+++ src/components/Navigation/index.js
@@ -1,25 +1,50 @@
 import { Link } from 'react-router-dom'
 import styles from './Navigation.module.css'
 
+import { Navbar, Nav, NavDropdown } from 'react-bootstrap'
+
 function Navigation(props){
     return (
-        <nav>
-        <ul>
-            { //javascript expression
-                props.user ?
-                <>
-                <li><Link to="" onClick={props.handleLogout}>Logout</Link></li>
-                <li><Link to="/dashboard">Dashboard</Link></li>
-                </>
-                :
-                <>
-                <li><Link to="/login">Login</Link></li>
-                <li><Link to="signup">Sign Up</Link></li>
-                </>
-            }
-        </ul>
-    </nav>
+        <Navbar collapseOnSelect expand="lg">
+        { props.user?
+        <Nav><Link to="/dashboard">{props.user.firstName}'s Application Tracker</Link></Nav>
+        :
+        <Nav><Link to="/">Application Tracker</Link></Nav>
+        }
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
+            <Nav className="mr-auto">
+            </Nav>
+            <Nav className={styles.navItems}>
+        { props.user ?
+            <>
+            <Nav><Link to="" onClick={props.handleLogout}>Logout</Link></Nav>
+            <Nav><Link to="/dashboard">Dashboard</Link></Nav>
+            </>
+            :
+            <>
+            <Nav><Link to="/login">Login</Link></Nav>
+            <Nav><Link to="signup">Sign Up</Link></Nav>
+            </>
+        }
+            </Nav>
+        </Navbar.Collapse>
+        </Navbar>
     )
 }
 
 export default Navigation;
+
+/*
+
+    <header className={styles.Header}>
+
+    </header>
+
+        <nav>
+        <ul>
+
+        </ul>
+    </nav>
+
+    */
