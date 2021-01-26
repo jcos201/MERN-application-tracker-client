de49be5a7ebdaac3156c9b62f9b154f707656e6d
Jorge L. Cossi <jcos201@gmail.com>
2021-01-26 11:22:15 -0500

    add Boostrap styling to ApplicationRow

--- src/pages/DashboardPage/Applications/ApplicationRow/index.js
+++ src/pages/DashboardPage/Applications/ApplicationRow/index.js
@@ -1,4 +1,9 @@
 import { Link } from 'react-router-dom'
+import { Button, Accordion, Card, Col, Row } from 'react-bootstrap'
+import { Trash } from 'react-bootstrap-icons'
+
+import styles from './ApplicationRow.module.css'
+
 
 function ApplicationRow (props) {
     let id = props.appId;
@@ -7,14 +12,10 @@ function ApplicationRow (props) {
     
     if(props.interviewDate)  {
         interviewDate += `${formatDate(props.interviewDate, 2)}`;
-        console.log(interviewDate)
-
     }
 
     if(props.dateApplied)  {
         dateApplied += `${formatDate(props.dateApplied)}`;
-        console.log(dateApplied)
-
     }
 
     function formatDate(date, output) {
@@ -29,24 +30,35 @@ function ApplicationRow (props) {
             month = '0' + month;
         if (day.length < 2) 
             day = '0' + day;
-        
-        console.log(`dayte = ${date}`)
-    
+            
         return output === 2 ? [week[dayOfWeek], [month, day, year].join('/')].join(' ') : [month, day, year].join('/');
 
     }
 
-    return (        
-            <tr>
-            <th>{props.companyName}</th>
-            <th>{props.jobTitle}</th>
-            <th>{dateApplied}</th>
-            <th>{interviewDate}</th>
-            <th>{props.contactName}</th>
-            <th>{props.notes}</th>
-            <td><Link to={"/edit/"+id}><button>Edit</button></Link></td>
-            <td><Link to={"/delete/"+id}><button >Delete</button></Link></td>
-            </tr>)
+    return (      
+        <Accordion className={styles.accordionScroll}>
+            <Card>
+                <Accordion.Toggle as={Card.Header} eventKey="0" aria-expanded="false">
+                <strong>Company Name:</strong> {props.companyName}  <br/><strong>Position Applied For:</strong> {props.jobTitle}
+                </Accordion.Toggle>
+                <Accordion.Collapse eventKey="0">
+                    <Card.Body>
+                        <Row>
+                            <Col>Date Applied: <br/>{dateApplied} </Col>
+                            <Col>Date of Interview: <br/>{interviewDate}</Col>
+                            <Col>Contact Person: <br/>{props.contactName} </Col>
+                        </Row>
+                        <Row>
+                        <Col><br/>Notes: {props.notes}</Col>
+
+                        </Row>
+      <br/><Link to={"/edit/"+id}><Button className={styles.btnEdit} class="btn btn-primary btn-sm">Edit</Button></Link>  
+      <Link to={"/delete/"+id}><Button className={styles.btnDanger} class="btn btn-danger btn-sm"><Trash /></Button></Link>
+                    </Card.Body>
+                </Accordion.Collapse>
+            </Card>
+        </Accordion>
+            )
 };
 
 export default ApplicationRow;
