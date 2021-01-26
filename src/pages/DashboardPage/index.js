33a9de59dfe71afbdb034a0be4e292b7d737e713
Jorge L. Cossi <jcos201@gmail.com>
2021-01-25 20:00:46 -0500

    fixed text align in cols

--- src/pages/DashboardPage/index.js
+++ src/pages/DashboardPage/index.js
@@ -7,12 +7,17 @@ function DashboardPage (props) {
         <Container className={styles.page}> 
             <Row className={styles.text}>{props.user.firstName}'s Dashboard Page</Row>
             <Row xs={1} md={2} className={styles.rowCol}>
-            <Col className={styles.article}>
-            <Link to="/jobsearch" ><article>My Job Search</article></Link>
+            
+            <Link to="/jobsearch" className={styles.article}>
+            <Col className={styles.colText}>
+                <article>My Job Search</article>
             </Col>
-            <Col  className={styles.article}>
-            <Link to="/applications"><article>Applications</article></Link>
+            </Link>
+            <Link to="/applications" className={styles.article}>
+            <Col  className={styles.colText}>
+                <article>Applications</article>
             </Col>
+                </Link>
             </Row>
         </Container>
     )
