a568270a68919adb502e2822ea491e3a8db180d2
Jorge L. Cossi <jcos201@gmail.com>
2021-04-24 06:41:55 -0400

    Adds documentation and styling for signup page

--- src/pages/SignupPage/index.js
+++ src/pages/SignupPage/index.js
@@ -15,6 +15,10 @@ function SignupPage (props) {
         password: "",
     }}
 
+    /* 
+    handleChange = every time keydown on input fields: 
+        spread out previous form state and update field keydown was initiated on
+    */
     function handleChange (event) {
         setFormState(prevState => ({
             ...prevState,
@@ -22,6 +26,18 @@ function SignupPage (props) {
         }))
     }
 
+    /*
+    Asynchronous function to handle submission of login data
+
+    Try / Catch will test for errors in input fields.
+        - If error alert user to error
+        - If successful:
+            - Await signup function from services.js to execute to create new user and token
+            - Clear signup form
+            - handlSignupOrLogin function will get newly created user and set user state to newly created user
+                this will allow user's application and job search entries to be saved to this specific user
+            - render dashboard landing page for newly created user
+    */
     async function handleSubmit (event) {
         try {
             event.preventDefault();
@@ -41,45 +57,42 @@ function SignupPage (props) {
     return(
         <Container>
             <Row className={styles.formRow}>
-            <Col sm={1} md={1} lg={3}></Col>
-            <Col className="formDiv"  sm={3} md={6} lg={5}>
-            
-            <form className={styles.formBox}>
-                <div><input 
-                value={formState.firstName} 
-                onChange={handleChange} 
-                placeholder="First Name"
-                name="firstName" 
-                type="text"
-                required />
-                </div>
-                <div><input 
-                value={formState.lastName} 
-                onChange={handleChange} 
-                placeholder="Last Name"
-                name="lastName" 
-                type="text" />
-                </div>
-                <div><input 
-                value={formState.email} 
-                onChange={handleChange} 
-                placeholder="Email"
-                name="email" 
-                type="email"
-                required />
-                </div>
-                <div><input 
-                value={formState.password} 
-                onChange={handleChange} 
-                placeholder="Password"
-                name="password" 
-                type="password"
-                required />
-                </div>
-                <Button className={styles.button} onClick={handleSubmit}>Sign Up</Button>
-            </form>
-        </Col>
-            <Col sm={1} md={1} lg={3}></Col>
+                <Col className="formDiv"  sm={12} md={6} lg={6}>
+                    <form className={styles.formBox}>
+                        <div><input 
+                        value={formState.firstName} 
+                        onChange={handleChange} 
+                        placeholder="First Name"
+                        name="firstName" 
+                        type="text"
+                        required />
+                        </div>
+                        <div><input 
+                        value={formState.lastName} 
+                        onChange={handleChange} 
+                        placeholder="Last Name"
+                        name="lastName" 
+                        type="text" />
+                        </div>
+                        <div><input 
+                        value={formState.email} 
+                        onChange={handleChange} 
+                        placeholder="Email"
+                        name="email" 
+                        type="email"
+                        required />
+                        </div>
+                        <div><input 
+                        value={formState.password} 
+                        onChange={handleChange} 
+                        placeholder="Password"
+                        name="password" 
+                        type="password"
+                        required />
+                        </div>
+                        <Button className={styles.button} onClick={handleSubmit}>Sign Up</Button>
+                    </form>
+                </Col>
             </Row>
         </Container>
     );
