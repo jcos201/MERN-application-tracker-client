f8c404284cb785925934d238497e675e11885bfa
Jorge L. Cossi <jcos201@gmail.com>
2021-05-06 06:33:15 -0400

    Adds homepage copy, animation and mobile-first styling

--- src/pages/HomePage/index.js
+++ src/pages/HomePage/index.js
@@ -1,16 +1,36 @@
 import logo from '../../ApplicationTracker.png'
 import styles from './HomePage.module.css'
 
-import { Container, Row } from 'react-bootstrap'
+import { Row, Col } from 'react-bootstrap'
+import Fade from 'react-reveal/Fade'
 
 function HomePage (props) {
     return(
-        <Container className={styles.div}>
-            <Row>An online resource to help with your job search.</Row>
-            <Row>Search Jobs, save your searches and track applications you've submitted.</Row>
-            <Row>Sign up or Login to continue.</Row>
-            <img src={logo} className={styles.img} alt="Search Jobs, save your searches and track applications you've submitted."/>
-        </Container>
+        <>
+        <Row className={styles.row}><h3>An online resource to help with your job search.</h3></Row>
+        <Col className={styles.text}>
+            <Row>
+                <Col sm={ {span:7, order:'first' }} xs={{ span:18, order: 'last' }}>
+                    <Fade duration={3000}>
+                    <img src={logo} className={styles.img} alt="Search Jobs, save your searches and track applications you've submitted."/>
+                    </Fade>
+                </Col>
+                <Col sm={{ span:5, order:'last' }} xs={{ span:18, order: 'first' }} className={styles.h4Col}> 
+                    <Fade delay={1000} right cascade duration={1500}>
+                        <Row className={styles.animatedText}>Search Jobs.</Row>
+                    </Fade>
+                    <Fade delay={1500} right cascade duration={1500}>
+                        <Row className={styles.animatedText}>Save those searches.</Row>
+                    </Fade>
+                    <Fade delay={2000} right cascade duration={1500}>
+                        <Row className={styles.animatedText}>Track applications submitted.</Row>
+                    </Fade>
+                </Col>
+            </Row>
+            <hr/>
+            <Row className={styles.row}><h3>Sign up or Login to continue.</h3></Row>
+        </Col>
+        </>
     )
 
 }
